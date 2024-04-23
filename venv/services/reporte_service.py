f56692ab3caf6904501929d47ee71bd7a9e8f4e1
hector.zuniga.luarte <hector.zuniga.luarte@gmail.com>
2024-04-23 15:10:32 -0400

    Se sube los archivos de servicios que quedaron rezagados por error.

--- venv/services/reporte_service.py
+++ venv/services/reporte_service.py
@@ -3,6 +3,7 @@ from typing import List
 from infrastructure.constants import APITaller
 import httpx
 from httpx import Response
+from datetime import date
 
 
 async def get_reporte_valorizacion_taller(cod_reporte: int, id_usuario: int) -> List[dict]:
@@ -61,4 +62,38 @@ async def get_reporte_asignacion_registro_docentes(cod_reporte: int, ano_academ:
         row["porcentaje_registro"] = None if row["total_taller_asignado"] == 0 else round(row["total_taller_registrado"] / row["total_taller_asignado"], 2)
 
     # Si todo está correcto, Retornamos la respuesta de la API
+    return reporte
+
+
+async def get_reporte_resumen_producto_rango_fechas(cod_reporte: int, ano_academ: int, fecha_inicio: date, fecha_termino: date, id_usuario: int) -> List[dict]:
+    # Armamos la URL de la API respectiva
+    url = f"{APITaller.URL_BASE.value}/consulta/{cod_reporte}/ano_academ/{ano_academ}/fecha_inicio/{fecha_inicio}/fecha_termino/{fecha_termino}/{id_usuario}"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response: Response = await client.get(url)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            raise Exception(f"Error en la llamada a la API respectiva. [{str(e)}]")
+        except httpx.RequestError as e:
+            raise Exception(f"Error de conexión con la API respectiva. [{str(e)}]")
+
+    reporte = response.json()
+    return reporte
+
+
+async def get_reporte_detalle_producto_taller_rango_fechas(cod_reporte: int, ano_academ: int, fecha_inicio: date, fecha_termino: date, id_usuario: int) -> List[dict]:
+    # Armamos la URL de la API respectiva
+    url = f"{APITaller.URL_BASE.value}/consulta/{cod_reporte}/ano_academ/{ano_academ}/fecha_inicio/{fecha_inicio}/fecha_termino/{fecha_termino}/{id_usuario}"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response: Response = await client.get(url)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            raise Exception(f"Error en la llamada a la API respectiva. [{str(e)}]")
+        except httpx.RequestError as e:
+            raise Exception(f"Error de conexión con la API respectiva. [{str(e)}]")
+
+    reporte = response.json()
     return reporte
